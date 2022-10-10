af3393a6cbdd8a009d1a2bca0f5fc1af81d7ee6c
harold <haroldnelsonduque@hotmail.com>
2022-10-10 10:27:58 -0500

    Primer commit

---
+++ webpack.prod.js
@@ -0,0 +1,85 @@
+const HtmlWebPack = require('html-webpack-plugin');
+const MiniCssExtract = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+const CssMinimizer =  require("css-minimizer-webpack-plugin");
+const Terser =  require("terser-webpack-plugin");
+
+
+module.exports = { 
+    mode: "production", 
+
+    output:{
+        // limpia todo y lo vuelve en el modo inicial
+        clean: true,
+        filename: 'main.[contenthash].js'
+
+    },
+
+    module: {
+        rules: [
+            {
+
+                test: /\.html$/,
+                loader: 'html-loader',
+                options: {
+                    sources: false
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude:/style.css$/,
+                use: ['style-loader','css-loader']
+            },
+            {
+                test:/style.css$/,
+                use: [MiniCssExtract.loader, 'css-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'file-loader'
+            },
+
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: "babel-loader",
+                  options: {
+                    presets: ['@babel/preset-env']
+                  }
+                }
+            }
+        ]
+    },
+
+    optimization: {
+       minimize: true, 
+       minimizer:[
+        new CssMinimizer(),
+        new Terser(), 
+       ]
+    },
+
+    plugins: [
+        new HtmlWebPack({
+            title: 'Mi Webpack App',
+            //filename: 'index.html'
+            template:'./src/index.html'
+        }),
+
+        new MiniCssExtract({
+            filename: '[name].[fullhash].css',
+            ignoreOrder: false
+        }),
+
+        new CopyPlugin({
+            patterns:[
+            {from:'src/assets', to: 'assets/'}
+        ]
+        }
+        )
+    ]
+
+    
+};
